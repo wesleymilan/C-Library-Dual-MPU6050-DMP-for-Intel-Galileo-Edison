718d4621136a42d62e5527a60578dcc87b0e5f9c
Wesley Milan <wesleybatista@gmail.com>
2015-04-28 16:45:47 -0300

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # C-Library-MPU6050-DMP-for-Intel-Galileo-Edson
 This is a C++ library for MPU6050 using DMP for Intel Galileo and Intel Edison for IoT
+
+This is a extended version of Jeff Rowberg library for MPU6050.
+
+I'm using I2Cdev library from http://www.i2cdevlib.com/.
+
+This library was tested with Galileo Gen 1 where interruption detection just works in EDGE_BOTH mode, so I'm not using interrupt detection to improve performance of I2C network.
+
+The repository have 3 executables:
+- demo_raw: Print the pure RAW data from Gyro to help you find the offset number from each axe
+- demo_dmp: The original example that execute each step for connection, accessing and catching data from gyro
+- demo_optimized: Optimized version using abstracted functions to simplify main code and reduce the I2C requests
+
+The original version made one request for each cycle of your application, increasing the CPU utilization and the I2C traffic.
+An empty request just for check if FIFO Buffer is done to load expends between 2ms and 6ms, and a load request uses between 6ms and 12ms of your cycle time, but the average time between each effective data load is 30ms, so, you can use this time to do a lot of things in your application including process data and capture information from other sensors. The getIntDataReadyStatus() supposed to get information from the interrupt port, but it makes a request through I2C taking the same 2~6ms to get the answer, so it is not good enough for me.
+
+The optimized version does not use interrupt yet, so if you want to help, please be my guest to implement and send me a pull request.
+
+A time average function monitor the interval between each request and make your application avoid empty requests releasing your CPU during 8 to 24 cycles.  You can test it's efficiency using Debug mode.
+
+I need your help to improve this library:
+
+TODO list:
+- Reorganize the functions and remove function code from header files
+- Implement interruption detection even using time functions to accurate the performance and avoid data loss
+- Make it compatible with other boards like RaspBerryPI, Edison, PCDuino, etc
+
+Enjoy
---
+++ demo_optimized.cpp
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <string.h>
+#include <math.h>
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+
+MPU6050 mpu_head;
+MPU6050 mpu_body;
+
+//#define OUTPUT_READABLE_QUATERNION
+#define OUTPUT_READABLE_EULER
+#define OUTPUT_READABLE_YAWPITCHROLL
+#define OUTPUT_READABLE_REALACCEL
+#define OUTPUT_READABLE_WORLDACCEL
+//#define OUTPUT_TEAPOT
+
+// MPU control/status vars
+bool dmpHeadReady = false;  // set true if DMP init was successful
+bool dmpBodyReady = false;  // set true if DMP init was successful
+
+// orientation/motion vars
+Quaternion dmpQuat;         // [w, x, y, z]         quaternion container
+VectorInt16 dmpAccel;       // [x, y, z]            accel sensor measurements
+VectorInt16 dmpAccelReal;   // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 dmpAccelWorld;  // [x, y, z]            world-frame accel sensor measurements
+VectorFloat dmpGravity;     // [x, y, z]            gravity vector
+float dmpEuler[3];          // [psi, theta, phi]    Euler angle container
+float dmpYawPitchRoll[3];   // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+
+// ================================================================
+// ===                      INITIAL SETUP                       ===
+// ================================================================
+void setup() {
+    //mpu_head.setDebug(true);
+    dmpHeadReady = mpu_head.dmpStartDevice(0x68, 0, 0, 0);
+    
+    //mpu_body.setDebug(true);
+    dmpBodyReady = mpu_body.dmpStartDevice(0x69, 0, 0, 0);
+}
+
+// ================================================================
+// ===                    MAIN PROGRAM LOOP                     ===
+// ================================================================
+void loop() {
+    if(mpu_head.dmpGetData())
+    {
+        dmpQuat = mpu_head.getDmpQuaternion();
+        printf("quat %7.2f %7.2f %7.2f %7.2f    ", dmpQuat.w,dmpQuat.x,dmpQuat.y,dmpQuat.z);
+        
+        dmpAccel = mpu_head.getDmpAccel();
+        printf("areal %6d %6d %6d    ", dmpAccel.x, dmpAccel.y, dmpAccel.z);
+        
+        dmpAccelReal = mpu_head.getDmpAccelReal();
+        printf("areal %6d %6d %6d    ", dmpAccelReal.x, dmpAccelReal.y, dmpAccelReal.z);
+        
+        dmpAccelWorld = mpu_head.getDmpAccelWorld();
+        printf("aworld %6d %6d %6d    ", dmpAccelWorld.x, dmpAccelWorld.y, dmpAccelWorld.z);
+        
+        dmpGravity = mpu_head.getDmpGravity();
+        printf("areal %6d %6d %6d    ", dmpGravity.x, dmpGravity.y, dmpGravity.z);
+        
+        dmpEuler = mpu_head.getDmpEuler();
+        printf("euler %7.2f %7.2f %7.2f    ", dmpEuler[0], dmpEuler[1], dmpEuler[2]);
+        
+        dmpYawPitchRoll = mpu_head.getDmpYawPitchRoll();
+        printf("ypr  %7.2f %7.2f %7.2f    ", dmpYawPitchRoll[0], dmpYawPitchRoll[1], dmpYawPitchRoll[2]);
+    }
+    
+    if(mpu_body.dmpGetData())
+    {
+        dmpQuat = mpu_body.getDmpQuaternion();
+        printf("quat %7.2f %7.2f %7.2f %7.2f    ", dmpQuat.w,dmpQuat.x,dmpQuat.y,dmpQuat.z);
+        
+        dmpAccel = mpu_body.getDmpAccel();
+        printf("areal %6d %6d %6d    ", dmpAccel.x, dmpAccel.y, dmpAccel.z);
+        
+        dmpAccelReal = mpu_body.getDmpAccelReal();
+        printf("areal %6d %6d %6d    ", dmpAccelReal.x, dmpAccelReal.y, dmpAccelReal.z);
+        
+        dmpAccelWorld = mpu_body.getDmpAccelWorld();
+        printf("aworld %6d %6d %6d    ", dmpAccelWorld.x, dmpAccelWorld.y, dmpAccelWorld.z);
+        
+        dmpGravity = mpu_body.getDmpGravity();
+        printf("areal %6d %6d %6d    ", dmpGravity.x, dmpGravity.y, dmpGravity.z);
+        
+        dmpEuler = mpu_body.getDmpEuler();
+        printf("euler %7.2f %7.2f %7.2f    ", dmpEuler[0], dmpEuler[1], dmpEuler[2]);
+        
+        dmpYawPitchRoll = mpu_body.getDmpYawPitchRoll();
+        printf("ypr  %7.2f %7.2f %7.2f    ", dmpYawPitchRoll[0], dmpYawPitchRoll[1], dmpYawPitchRoll[2]); 
+    }
+}
+
+int main() {
+    setup();
+    usleep(100000);
+    
+    for (;;)
+        loop();
+    
+    return 0;
+}
+
